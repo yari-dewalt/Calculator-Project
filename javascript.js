362ed2163b412a9a44a635a2933d6e3508d4ceef
yari-dewalt <yari.dewalt@gmail.com>
2023-05-30 20:27:17 -0700

    Create operational functions

---
+++ javascript.js
@@ -0,0 +1,30 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+function operate(a, op, b) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "ERROR: Invalid Input";
+    }
+}
